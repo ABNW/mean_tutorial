c175120bf011b8821c61c539f652db833196bdc6
Miles <miles.tincknell@gmail.com>
2015-02-26 00:04:06 +0000

    Created a server that serves a static html page :/

---
+++ testTwo/server.js
@@ -0,0 +1,23 @@
+// get the HTTP and filesystem modules
+var http = require('http'),
+    fs   = require('fs');
+
+//create our server using the http module
+http.createServer(function(req, res){
+
+  //write to our server. Set configuration for the response
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+    'Access-Control-Allow-Origin' : '*'
+  });
+
+  //grab the index.html file using fs
+  var readStream = fs.createReadStream(__dirname + '/index.html');
+
+  //send the index.html file to our user
+  readStream.pipe(res);
+
+}).listen(1337);
+
+//Tell ourselves what's happenin
+console.log('Visit me at http://localhost:1337');
